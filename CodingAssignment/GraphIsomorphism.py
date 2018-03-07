c021b80ac90b4a26e09a988437008a5e09f1da5f
Mathieu <m.nass@student.utwente.nl>
2018-03-07 16:27:26 +0100

    Starting to work

--- CodingAssignment/GraphIsomorphism.py
+++ CodingAssignment/GraphIsomorphism.py
@@ -5,4 +5,15 @@ with open('colorref_smallexample_4_7.grl') as f:
     L = load_graph(f, read_list = True)
 
 def compare_graphs(L):
-    
+    for x in L[0]:
+        for y in x.vertices:
+            y.colornum = len(y.incidence)
+            print('node ' + str(y) + ' has ' + str(y.colornum) + ' connections')
+
+
+
+compare_graphs(L)
+
+G = L[0][0]
+with open('mygraph.dot', 'w') as f:
+    write_dot(G, f)
